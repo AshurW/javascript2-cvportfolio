78016f302ccc350d9a785f8d4ac466c60c343cb0
ashurw <ashurw@gmail.com>
2019-11-15 14:25:21 +0100

    more styling and changed som boostrap default

--- js/main.js
+++ js/main.js
@@ -1,18 +1,25 @@
-$(function() {
+$(function () {
+    //hide the project and about me Div at the start
     $("#project, #about").hide();
 
-    $("#homeLink, #projectLink, #aboutLink").bind("click", function() {
-        // $("#home, #project, #about").hide();
-        if ($(this).attr("id") == "projectLink") {
+    $("#homeLink, #projectLink, #aboutLink, #cardProjectLink, #cardAboutLink").bind("click", function () {
+        // checks what you have clicked on the navbar remove the other div and show the one you
+        // have selected and also buts an active class to indicated in the navbar where you are
+        if ($(this).attr("id") == "projectLink" || $(this).attr("id") == "cardProjectLink") {
             $("#home, #about").hide();
+            $("#homeLink, #aboutLink").removeClass("active");
             $("#project").show();
-        } else if ($(this).attr("id") == "aboutLink"){
+            $("#projectLink").addClass("active");
+        } else if ($(this).attr("id") == "aboutLink" || $(this).attr("id") == "cardAboutLink") {
             $("#home, #project").hide();
-            $(" #about").show();
+            $("#homeLink, #projectLink").removeClass("active");
+            $("#about").show();
+            $("#aboutLink").addClass("active");
         } else {
             $("#project , #about").hide();
+            $("#projectLink, #aboutLink").removeClass("active");
             $("#home").show();
+            $("#homeLink").addClass("active");
         }
-    })
-
+    });
 });
